5cc637b10dd539ce144016824052ed16905503d8
Jose Ferreira <jpedroferreira95@gmail.com>
2026-01-07 11:00:28 +0000

    feat: update env file

--- project/src/config/env.ts
+++ project/src/config/env.ts
@@ -1,4 +1,5 @@
-export const CALENDAR_INFO =  import.meta.env.VITE_CALENDAR_INFO;
-export const MAP_INFO = import.meta.env.VITE_MAP_INFO;
+export const CALENDAR_INFO =
+  "https://calendar.google.com/calendar/u/0/r/day/2026/7/11?tab=mc&date=20081101";
+export const MAP_INFO = "https://maps.app.goo.gl/uyJFurUmVWxohYU67";
 export const JOSE_CONTACT = import.meta.env.VITE_JOSE_CONTACT;
 export const INES_CONTACT = import.meta.env.VITE_INES_CONTACT;
