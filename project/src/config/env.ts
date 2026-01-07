7b9fb8f37ee01f55fa48f3780be942749d91759c
Jose Ferreira <jpedroferreira95@gmail.com>
2026-01-07 10:56:28 +0000

    feat refactor env file

--- project/src/config/env.ts
+++ project/src/config/env.ts
@@ -1,4 +1,4 @@
-export const CALENDAR_INFO = import.meta.env.VITE_CALENDAR_INFO;
+export const CALENDAR_INFO =  import.meta.env.VITE_CALENDAR_INFO;
 export const MAP_INFO = import.meta.env.VITE_MAP_INFO;
 export const JOSE_CONTACT = import.meta.env.VITE_JOSE_CONTACT;
 export const INES_CONTACT = import.meta.env.VITE_INES_CONTACT;
